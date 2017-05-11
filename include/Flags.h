16f561aef0b868612b76171965af6dbfd16c77b4
JohnBobSmith <myrunescapeemail609@gmail.com>
2017-05-11 17:47:13 -0600

    Added flags class, code cleansing

---
+++ include/Flags.h
@@ -0,0 +1,24 @@
+#ifndef FLAGS_H
+#define FLAGS_H
+
+#include "../include/Collision.h"
+#include "../include/Camera.h"
+#include "../include/Player.h"
+#include "../include/Config.h"
+
+class Flags
+{
+    public:
+        //Position our flags collision boxes
+        void positionFlags(Collision &collision);
+
+        //Check for collisions
+        void checkCollision(Collision &collision, Camera &camera, Player &player, Config &config);
+
+    private:
+        //Change our level internally.
+        bool isLevelChanged = false;
+        void changeLevel(Config &config);
+};
+
+#endif // FLAGS_H

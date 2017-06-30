1e169fac7533f7f30d91fb98595b0aa52846f7cd
JohnBobSmith <myrunescapeemail609@gmail.com>
2017-06-30 17:00:35 -0600

    Purged a now obsolete variable

--- include/Config.h
+++ include/Config.h
@@ -17,15 +17,13 @@ class Config
         int getScreenHeight() { return screenHeight; }
         sf::Vector2i getScreenSize() { return screenSize; }
 
-        //The level that is loaded
-        std::string LEVEL_STRING = "rockland";
-
         //The objects in our tilemap to collide with
         const std::unordered_set<int> objectsInGrassland = {0, 3, 6, 7, 10, 11, 12};
         const std::unordered_set<int> objectsInFarmland = {0, 3, 7, 8, 9, 10, 11, 13, 14, 15};
         const std::unordered_set<int> objectsInAnimprop = {1, 4};
         const std::unordered_set<int> objectsInRockland = {0, 3, 5, 10, 11, 14, 15};
 
+        //Audio collision boxes
         const std::unordered_set<int> grasslandGrass = {1, 4, 5, 8, 9};
         const std::unordered_set<int> grasslandShrubs = {2};
         const std::unordered_set<int> farmlandGrass{1};
@@ -36,7 +34,7 @@ class Config
 
     private:
         //The window name
-        std::string appName = "World Testing";
+        std::string appName = "The Adventures of Stickman";
         //Screen dimensions
         int screenWidth = 480;
         int screenHeight = 480;
